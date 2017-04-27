ddd79d27251483950cf694b7a3af6c12f7613f70
prateekgogia <prateekgogia@hotmail.com>
2017-04-27 16:45:05 -0700

    added 2 new apis KeepWatch and MapContents

--- parse_ini.go
+++ parse_ini.go
@@ -1,10 +1,12 @@
 package ini
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/golang/glog"
 	"gopkg.in/fsnotify.v1"
 	"gopkg.in/ini.v1"
-	"sync"
 )
 
 type updateFunc func()
@@ -13,9 +15,14 @@ type IniFile interface {
 	// Return value from INI file, or the supplied default
 	ReadKey(section, key, defaultVal string) string
 	Register(f updateFunc)
+	KeepWatch() error
+	MapContents(v interface{}) error
 }
 
-var updaters map[string][]updateFunc
+var (
+	updaters map[string][]updateFunc
+	mutex    sync.RWMutex
+)
 
 type fileDetails struct {
 	fileName string
@@ -25,7 +32,6 @@ type fileDetails struct {
 }
 
 var iniFiler map[string]IniFile
-
 
 // Loads a new INI file and optionally watches file for changes
 func NewIniFile(fileName string) (IniFile, error) {
@@ -43,7 +49,7 @@ func NewIniFile(fileName string) (IniFile, error) {
 		updaters = make(map[string][]updateFunc)
 	}
 	if iniFiler == nil {
-		iniFiler =  make(map[string]IniFile)
+		iniFiler = make(map[string]IniFile)
 	}
 	iniFiler[fileName] = fd
 	return fd, nil
@@ -114,6 +120,8 @@ func (fd *fileDetails) keepWatch() error {
 
 func (fd *fileDetails) update() {
 
+	mutex.Lock()
+	defer mutex.Unlock()
 	updateFunc := updaters[fd.fileName]
 
 	if updateFunc == nil {
@@ -121,11 +129,68 @@ func (fd *fileDetails) update() {
 		return
 	}
 	for _, cbUpdateFunc := range updateFunc {
-		cbUpdateFunc()  /* Loop through all the registered functions */
+		go cbUpdateFunc() /* Loop through all the registered functions */
 	}
 }
 
 func (fd *fileDetails) Register(f updateFunc) {
 	/* Adding all the register function in an array */
+	mutex.Lock()
+	defer mutex.Unlock()
+	if updaters == nil {
+		updaters = make(map[string][]updateFunc)
+	}
 	updaters[fd.fileName] = append(updaters[fd.fileName], f)
+}
+
+// LoadIniConfig loads the INI config in the object c provided
+func LoadIniConfig(fileName string, c interface{}) (IniFile, error) {
+	cfg, err := ini.Load(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed load config file, err: %v", err)
+	}
+
+	f := &fileDetails{fileName: fileName, cfg: cfg}
+	if err = f.MapContents(c); err != nil {
+		return nil, fmt.Errorf("failed to map cfg err: %v", err)
+	}
+	return f, nil
+}
+
+// maps the content of INI file specified in fd to v.
+func (fd *fileDetails) MapContents(v interface{}) error {
+	var err error
+	fd.cfg, err = ini.Load(fd.fileName)
+	if err != nil {
+		return fmt.Errorf("failed load config file, err: %v", err)
+	}
+	return fd.cfg.MapTo(v)
+}
+
+func (fd *fileDetails) KeepWatch() error {
+	return fd.newKeepWatch()
+}
+
+func (fd *fileDetails) newKeepWatch() error {
+	watchman, err := fsnotify.NewWatcher()
+	if err != nil {
+		return fmt.Errorf("failed to create new watcher for INI file err: %v", err)
+	}
+	go func() {
+		for {
+			select {
+			case event := <-watchman.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					fd.update() // callBackRegistered
+				} else {
+					continue // ignore file event if its not Write
+				}
+			}
+		}
+	}()
+	err = watchman.Add(fd.fileName)
+	if err != nil {
+		return fmt.Errorf("Failed add new watcher for INI file err: %v", err)
+	}
+	return nil
 }
